df0943302e6db86dd047e25692baf4b4dfa05cb4
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-08-29 10:51:29 -0400

    update

--- BigORuleBook/Rule1Worstcase.js
+++ BigORuleBook/Rule1Worstcase.js
@@ -4,6 +4,7 @@ const nemo = ["nemo"];
 // When dealing with big O your always looking at the worst case scenario.
 // Meaning if we looped through this array we can find it in say the the first array, 
 // but worst case scenario we would have to loop through the entire array. meaning O(n)
+// were looking at scalability.
 function findNemo1(array) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
