dfc64cf811c8184c80a4503270913ee61805e1a9
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-08-29 10:40:28 -0400

    Update BigOExercise2.js

--- BigOExercise2.js
+++ BigOExercise2.js
@@ -1,16 +1,21 @@
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function anotherFunChallenge(input) {
-  let a = 5;
-  let b = 10;
-  let c = 50;
+  // were just assigning a variable to a function
+  let a = 5; // O(1)
+  let b = 10; // O(1)
+  let c = 50; // O(1)
   for (let i = 0; i < input; i++) {
-    let x = i + 1;
-    let y = i + 2;
-    let z = i + 3;
+    //   were just looping through the input
+    let x = i + 1; // O(n)
+    let y = i + 2; // O(n)
+    let z = i + 3; // O(n)
   }
   for (let j = 0; j < input; j++) {
-    let p = j * 2;
-    let q = j * 2;
+    let p = j * 2; // O(n)
+    let q = j * 2; // O(n)
   }
-  let whoAmI = "I don't know";
+  let whoAmI = "I don't know"; // O(1)
 }
+
+
+// Big O (4 + 5n)
