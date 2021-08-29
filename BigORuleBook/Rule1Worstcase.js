ebdb2350e2aa1efc6d658f44f99deb83d9b0f8a5
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-08-29 10:49:31 -0400

    Update Rule1Worstcase.js

--- BigORuleBook/Rule1Worstcase.js
+++ BigORuleBook/Rule1Worstcase.js
@@ -1,6 +1,9 @@
 //#1 -- For loop in Javascript.
 const nemo = ["nemo"];
 
+// When dealing with big O your always looking at the worst case scenario.
+// Meaning if we looped through this array we can find it in say the the first array, 
+// but worst case scenario we would have to loop through the entire array. meaning O(n)
 function findNemo1(array) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
