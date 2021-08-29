1063ddc63cd1217fb80668ed5160d208690ea1b6
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-08-29 11:09:26 -0400

    Create Rule3DiffrentTermsForInputs.js

---
+++ BigORuleBook/Rule3DiffrentTermsForInputs.js
@@ -0,0 +1,17 @@
+
+// Diffrent term for diffrent inputs    
+
+
+
+//Log all pairs of array
+
+const boxes = ["a", "b", "c", "d", "e"];
+function logAllPairsOfArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j]);
+    }
+  }
+}
+
+logAllPairsOfArray(boxes);
