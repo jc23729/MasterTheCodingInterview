1af13144d56dac5a6de0d346fb2634a16be785f1
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-08-29 08:24:49 -0400

    Update BigOExercise1.js

--- BigOExercise1.js
+++ BigOExercise1.js
@@ -1,12 +1,21 @@
 // What is the Big O of the below function? (Hint, you may want to go line by line)
 function funChallenge(input) {
-  let a = 10;
-  a = 50 + 3;
+  let a = 10;  // O(1) constant because were only running once
+  a = 50 + 3; // O(1) constant because were only running once
 
-  for (let i = 0; i < input.length; i++) {
-    anotherFunction();
-    let stranger = true;
-    a++;
+  for (let i = 0; i < input.length; i++) { // O(n) linear because we are looping through the input array
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
   }
-  return a;
+  return a; //0(1)
 }
+
+
+// 3 steps
+
+
+// 3 + n + n + n + n
+// BIG O(3 + 4n)
+
+
