d1543dbb94fb7dab757a5e2055273281c874eb8b
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-09-03 22:49:01 -0400

    Update BinaryTreeInorderTraversal.js

--- BigORuleBook/BinarySearch101/BinaryTreeInorderTraversal.js
+++ BigORuleBook/BinarySearch101/BinaryTreeInorderTraversal.js
@@ -1,6 +1,6 @@
 // Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
-
+// Examples
 // Input: root = [1, null, 2, 3];
 // Output: [1, 3, 2];
 
@@ -15,3 +15,25 @@
 
 // Input: root = [1, 2];
 // Output: [2, 1];
+
+
+// Constraints
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+
+var inorderTraversal = function (root) {
+  let result = [];
+  dfs(root);
+
+  function dfs(root) {
+    if (root != null) {
+      dfs(root.left);
+      result.push(root.val);
+      dfs(root.right);
+    }
+  }
+
+  return result;
+};
