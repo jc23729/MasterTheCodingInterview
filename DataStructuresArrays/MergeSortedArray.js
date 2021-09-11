32134b30f2ebdc77493f35b065d74f455a70518e
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-09-11 13:36:10 -0400

    Update MergeSortedArray.js

--- DataStructuresArrays/MergeSortedArray.js
+++ DataStructuresArrays/MergeSortedArray.js
@@ -1,34 +1,44 @@
+// function mergeSortedArrays(array1, array2) {
+//   const mergedArray = [];
+//   // grab our first item in the array
+//   let array1Item = array1[0];
+//   //    array2Item  grabs the 3 in the second array mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]);
+//   let array2Item = array2[0];
+//   let i = 1;
+//   let j = 1;
+
+//   //We should actually move these 2 if statements to line 2 so that we do the checks before we do assignments in line 3 and 4!
+//     // Check input
+//     // if first input is empty return second input
+//   if (array1.length === 0) {
+//     return array2;
+//   }
+//   if (array2.length === 0) {
+//     return array1;
+//   }
+
+//   while (array1Item || array2Item) {
+//     if (array2Item === undefined || array1Item < array2Item) {
+//       mergedArray.push(array1Item);
+//       array1Item = array1[i];
+//       i++;
+//     } else {
+//       mergedArray.push(array2Item);
+//       array2Item = array2[j];
+//       j++;
+//     }
+//   }
+//   return mergedArray;
+// }
+
+
 function mergeSortedArrays(array1, array2) {
+  // this is going to be our answer
   const mergedArray = [];
-  // grab our first item in the array
   let array1Item = array1[0];
-  //    array2Item  grabs the 3 in the second array mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]);
-  let array2Item = array2[0];
-  let i = 1;
-  let j = 1;
+  
 
-  //We should actually move these 2 if statements to line 2 so that we do the checks before we do assignments in line 3 and 4!
-    // Check input
-    // if first input is empty return second input
-  if (array1.length === 0) {
-    return array2;
-  }
-  if (array2.length === 0) {
-    return array1;
-  }
-
-  while (array1Item || array2Item) {
-    if (array2Item === undefined || array1Item < array2Item) {
-      mergedArray.push(array1Item);
-      array1Item = array1[i];
-      i++;
-    } else {
-      mergedArray.push(array2Item);
-      array2Item = array2[j];
-      j++;
-    }
-  }
-  return mergedArray;
 }
+
 
 mergeSortedArrays([0, 3, 4, 31], [3, 4, 6, 30]);
