04fbc278ac6e2b7f5b1b08d4ab4803c256fecc65
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-09-12 11:10:05 -0400

    Update linkedListConstructor.js

--- LinkedList/linkedListConstructor.js
+++ LinkedList/linkedListConstructor.js
@@ -1,13 +1,29 @@
 class Linkedlist extends Component {
-  // constructor (value) {..}
-  // push(value) {..}
-  // unshift(value) {..}
-  // insert (index, value) {..}
+    constructor(value) {
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = this.head
+        this.length = 1
+    }
 }
 
 
+// create new Node
+// create new Node
+// create new Node
+// create new Node
 
 // constructor (value) {..}
 // push(value) {..}  adds to the end of the list
 // unshift(value) {..} adds to the beginning of the list
 // insert (index, value) {..}  adds to the index of the value
+
+
+// class Linkedlist extends Component {
+//     constructor(value) {create new Node }
+//     push(value) {create new Node
+//     // add to the end of the list}
+//         unshift(value) {create new Node
+//         // adds Nodeto the beginning}
+//             insert(index, value) {create new Node insert Node}
+// }
