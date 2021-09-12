b99efd8af1dc15461969aa127eee66b6569ec2c8
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-09-12 11:25:27 -0400

    Update linkedListConstructor.js

--- LinkedList/linkedListConstructor.js
+++ LinkedList/linkedListConstructor.js
@@ -35,32 +35,33 @@
 // USING PUSH METHOD
 
 class Node {
-    constructor(value) {
-        this.value = value
-        this.next = null
-    }
-     }
-    
-
-
-class Linkedlist{
-    constructor(value) {
-        const newNode = new Node(value)
-        this.head = newNode
-        this.tail = this.head
-        this.length = 1
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
-push(value) {
-    const newNode = new Node(value)
-    if(!this.head) {
-        this.head = newNode
-        this.tail = this.head
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
     } else {
-        this.tail.next = newNode
-        this.tail = newNode
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    this.length++
-    return this 
+    this.length++;
+    return this;
+  }
 }
+
+let myLinkedList = new LinkedList(7);
+myLinkedList.push(4);
